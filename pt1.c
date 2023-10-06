3d01bba4d61d57f7c3bea0b84e8ae918fb278458
zeuscrx12 <saswataroyjpg@gmail.com>
2023-10-07 00:40:39 +0530

    diamond pattern

---
+++ pt1.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main() {
+    int n = 5;
+    for (int i = 1; i < n; i++) {
+        for (int j = i; j <= n; j++) {
+            printf("  ");
+        }
+        for (int j = 1; j < i; j++) {
+            printf("* ");
+        }
+        for (int j = 1; j <= i; j++) {
+            printf("* ");
+        }
+        printf("\n");
+
+    }
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            printf("  ");
+        }
+        for (int j = i; j < n; j++) {
+            printf("* ");
+        }
+        for (int j = i; j <= n; j++) {
+            printf("* ");
+        }
+        printf("\n");
+
+    }
+}
